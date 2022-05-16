4116fd18b4c8df9f599915b13c0787ca09666df7
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-16 09:57:43 +0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yuri Gerasimchuk on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
